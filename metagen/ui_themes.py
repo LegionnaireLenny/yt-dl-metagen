18d1fa9b2480eb14889e86dc673b5257d34b0c43
LegionnaireLenny <LegionnaireLenny@tutanota.com>
2025-09-12 19:41:32 -0400

    Added another ui theme

--- metagen/ui_themes.py
+++ metagen/ui_themes.py
@@ -46,7 +46,7 @@ text_input_names = [
     "playlist_item_title"
 ]
 
-light_theme = {
+light_tan_theme = {
     # Playlist item checkbox
     "playlist_item_checkbox": colormap["green"],
     # Playlist scrollbar
@@ -65,14 +65,14 @@ light_theme = {
     "title_bar_close_button_background": colormap["beige"],
 }
 
-set_widget_theme(theme=light_theme,
+set_widget_theme(theme=light_tan_theme,
                  widget_names=button_names,
                  widget_type="button",
                  background=colormap["beige"],
                  border=border_themes["border_medium"],
                  text=colormap["beige"])
 
-set_widget_theme(theme=light_theme,
+set_widget_theme(theme=light_tan_theme,
                  widget_names=text_input_names,
                  widget_type="text_input",
                  background= colormap["beige"],
@@ -81,17 +81,17 @@ set_widget_theme(theme=light_theme,
                  hint=colormap["darkgoldenrod"],
                  selection=modify_color("sandybrown", 1, 100))
 
-dark_theme = {
+blue_theme = {
     # Playlist item checkbox
     "playlist_item_checkbox": colormap["plum"],
     # Playlist scrollbar
-    "scrollbar_active": modify_color("mediumslateblue", 0.9 , 255),
+    "scrollbar_active": modify_color("mediumslateblue", 0.9, 255),
     "scrollbar_inactive": modify_color("mediumslateblue", 0.7, 255),
     # Window colors
     "window_background": modify_color("cornflowerblue", 0.4, 255),
     "title_bar_background": get_tinted_color(rgb_value=modify_color("cornflowerblue", 0.8, 255)),
     # Title bar previous action button
-    "title_bar_action_previous_text": modify_color("lightsteelblue", 1 , 255),
+    "title_bar_action_previous_text": modify_color("lightsteelblue", 1, 255),
     # Title bar minimize button
     "title_bar_minimize_button_background": colormap["beige"],
     # Title bar maximize button
@@ -100,20 +100,56 @@ dark_theme = {
     "title_bar_close_button_background": colormap["beige"],
 }
 
-set_widget_theme(theme=dark_theme,
+set_widget_theme(theme=blue_theme,
                  widget_names=button_names,
                  widget_type="button",
                  background=modify_color("cornflowerblue", 0.8, 255),
                  border=border_themes["border_medium"],
-                 text=modify_color("lightsteelblue", 1 , 255))
+                 text=modify_color("lightsteelblue", 1, 255))
 
-set_widget_theme(theme=dark_theme,
+set_widget_theme(theme=blue_theme,
                  widget_names=text_input_names,
                  widget_type="text_input",
-                 background=modify_color("cornflowerblue", 0.6 , 255),
+                 background=modify_color("cornflowerblue", 0.6, 255),
                  border=border_themes["border_medium"],
-                 text=modify_color("lightsteelblue", 1 , 255),
+                 text=modify_color("lightsteelblue", 1, 255),
                  hint=modify_color("darkslateblue", 0.5, 150),
                  selection=modify_color("skyblue", 1, 100))
 
-ui_theme = light_theme
+purple_theme = {
+    # Playlist item checkbox
+    "playlist_item_checkbox": colormap["plum"],
+    # Playlist scrollbar
+    "scrollbar_active": modify_color("mediumslateblue", 0.9, 255),
+    "scrollbar_inactive": modify_color("mediumslateblue", 0.7, 255),
+    # Window colors
+    "window_background": modify_color("darkslateblue", 0.6, 255),
+    "title_bar_background": get_tinted_color(rgb_value=modify_color("darkslateblue", 1.2, 255)),
+    # Title bar previous action button
+    "title_bar_action_previous_text": modify_color("gold", 0.7, 255),
+    # Title bar minimize button
+    "title_bar_minimize_button_background": colormap["beige"],
+    # Title bar maximize button
+    "title_bar_maximize_button_background": colormap["beige"],
+    # Title bar close button
+    "title_bar_close_button_background": colormap["beige"],
+}
+
+set_widget_theme(theme=purple_theme,
+                 widget_names=button_names,
+                 widget_type="button",
+                 background=modify_color("darkslateblue", 1.2, 255),
+                 border=border_themes["border_medium"],
+                 text=modify_color("gold", 0.7, 255))
+
+set_widget_theme(theme=purple_theme,
+                 widget_names=text_input_names,
+                 widget_type="text_input",
+                 background=modify_color("darkslateblue", 1, 255),
+                 border=border_themes["border_medium"],
+                 text=modify_color("gold", 0.7, 255),
+                 hint=modify_color("gold", 0.7, 150),
+                 selection=modify_color("skyblue", 1, 100))
+
+ui_theme = purple_theme
+
